c060da8f7849dea6e1b504597d1492d4413bdea9
tiltedbarbarian <79463050+tiltedbarbarian@users.noreply.github.com>
2022-08-11 19:35:33 +1000

    MonsterData
    a simple MonsterData file as an example for the data read/write

---
+++ MonsterProject/Assets/Scripts/Classes/MonsterData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster
+{
+    public int MonsterID;
+    public string Name;
+    public int combatPower;
+    
+}
