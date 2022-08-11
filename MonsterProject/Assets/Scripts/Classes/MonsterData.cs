dcd1d9dfbcae90eaa7329097eb7f8ad7ae24c91a
tiltedbarbarian <79463050+tiltedbarbarian@users.noreply.github.com>
2022-08-11 19:46:58 +1000

    basic

--- MonsterProject/Assets/Scripts/Classes/MonsterData.cs
+++ MonsterProject/Assets/Scripts/Classes/MonsterData.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Monster
+public class MonsterData
 {
     public int MonsterID;
     public string Name;
     public int combatPower;
-    
+
+    public MonsterData(int monID, string name, int cP)
+    {
+        MonsterID = monID;
+        Name = name;
+        combatPower = cP;
+
+    }
 }
---
+++ MonsterProject/Assets/Scripts/Classes/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string name;
+    public string playerid;
+    public MonsterData[] activeTeam;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
