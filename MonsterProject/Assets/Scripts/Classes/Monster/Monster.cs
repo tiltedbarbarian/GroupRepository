b4afe78de092ef798f8e09f0137bb39a7df37a1b
tiltedbarbarian <79463050+tiltedbarbarian@users.noreply.github.com>
2022-08-12 13:44:24 +1000

    push

--- MonsterProject/Assets/Scripts/Classes/Monster/Monster.cs
+++ MonsterProject/Assets/Scripts/Classes/Monster/Monster.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Monster", menuName = "Monsters")]
 public class Monster : ScriptableObject
 {
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    public int monsterID;
+    public string monsterName;
+    public int combatPower;
 
 }
--- MonsterProject/Assets/Scripts/Classes/Player.cs
+++ MonsterProject/Assets/Scripts/Classes/Player.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
 
 public class Player : MonoBehaviour
 {
-    public string name;
-    public string playerid;
-    public List<MonsterData> activeTeam;
+    public string playerName;
+    public string playerId;
+    public List<Monster> activeTeam;
+    public List<Monster> monsterStorage;
 
     void Start()
     {
+        try
+        {
+            using (StreamReader sr = new StreamReader("TestFile.txt"))
+            {
+                playerName = sr.ReadLine();
+                playerId = sr.ReadLine();
+
+                
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The File could not be read:");
+            Console.WriteLine(e.Message);
+        }
         
     }
 
