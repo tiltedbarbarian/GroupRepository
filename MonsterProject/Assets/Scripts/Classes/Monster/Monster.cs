5901a345bd7c085a3f0ae8d47e39b93e3893a6a3
jloweis <jaiden.oweis@students.mq.edu.au>
2022-08-12 13:11:03 +1000

    ScriptableObject (12/08/2022)

---
+++ MonsterProject/Assets/Scripts/Classes/Monster/Monster.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Monster", menuName = "Monsters")]
+public class Monster : ScriptableObject
+{
+
+}
