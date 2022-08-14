f7b560925a1dd7b1063e2758b90785476c55bb59
tiltedbarbarian <79463050+tiltedbarbarian@users.noreply.github.com>
2022-08-14 21:37:06 +1000

    canvas

--- MonsterProject/Assets/Scripts/StartScript.cs
+++ MonsterProject/Assets/Scripts/StartScript.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class StartScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int[] yourTeam = new int[3];
+    public int[] enemyTeam = new int[3];
+    public int mapType = 0;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
