fee6cca2dacd92e7ffd9ecdce19fb1b526e994f2
tiltedbarbarian <79463050+tiltedbarbarian@users.noreply.github.com>
2022-08-16 01:42:34 +1000

    got toggles working in scripts
    using UnityEngine.UI;

--- MonsterProject/Assets/Scripts/StartScript.cs
+++ MonsterProject/Assets/Scripts/StartScript.cs
@@ -1,21 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartScript : MonoBehaviour
 {
     public int[] yourTeam = new int[3];
     public int[] enemyTeam = new int[3];
     public int mapType = 0;
-    public GameObject panelLeft;
-    public GameObject panelRight;
-    
 
+    public ToggleGroup toggleGroupLeft;
+    public ToggleGroup toggleGroupRight;
+    private int countLeft = 0;
+    private int countRight = 0;
+
+    public void Start()
+    {
+        toggleGroupLeft.SetAllTogglesOff();
+        toggleGroupRight.SetAllTogglesOff();
+    }
     public void startPressed()
     {
-        int counter = 0;
-        bool isOn;
-        isOn = panelLeft.GetComponentInChildren<Toggle>.isOn();
+        
+
+
+
+
+        
+        
         
 
     }
